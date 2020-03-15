18c2e7ee84edbcab4fb5b3c5d1b8e82ee92e4b75
Omer Yair <omeryair@gmail.com>
2020-03-15 10:06:54 +0000

    Added list to ArgumentParser

--- gptb/config.py
+++ gptb/config.py
@@ -1,6 +1,4 @@
 import sys
-import os
-import inspect
 import argparse
 
 import json
@@ -10,29 +8,35 @@ class ArgumentParser(argparse.ArgumentParser):
     def __init__(self, *args, **kwargs):
         self.defaults_file_prefix_char = kwargs.pop('defaults_file_prefix_char', None)
         self._json_args = []
+        self._list_args = {}
         self._template_args = []
 
         super().__init__(*args, **kwargs)
 
     def add_argument(self, *args, **kwargs):
         is_json = ('type' in kwargs) and (kwargs['type'] == 'json')
+        is_list = ('type' in kwargs) and (kwargs['type'] == 'list')
         is_template = ('type' in kwargs) and (kwargs['type'] == 'template')
-        if is_json or is_template:
+        if is_json or is_template or is_list:
             kwargs['type'] = str
 
         action = super().add_argument(*args, **kwargs)
 
         if is_json:
             self._json_args.append(action.dest)
+        if is_list:
+            self._list_args[action.dest] = kwargs.get('sep', ',')
         if is_template:
             self._template_args.append(action.dest)
 
         return action
 
-    def parse_known_args(self, args, namespace):
+    def parse_known_args(self, args=None, namespace=None):
         # types = {}
         # for action in self._actions:
         #     types[action.dest] = action.type if (action.type is not None) else str
+
+        args_set = set(vars(super().parse_known_args()[0]).keys())
 
         ## Copyied from: https://github.com/python/cpython/blob/65dcc8a8dc41d3453fd6b987073a5f1b30c5c0fd/Lib/argparse.py#L1822
         if args is None:
@@ -55,11 +59,13 @@ class ArgumentParser(argparse.ArgumentParser):
             else:
                 try:
                     defaults = yaml.load(open(arg_string[1:]).read(), Loader=yaml.FullLoader)
+                    for key in (set(defaults.keys()) - args_set):
+                        print('!! Warning: agument "{}" might not be a valid argument'.format(key))
                     self.set_defaults(**defaults)
                 except OSError:
                     err = sys.exc_info()[1]
                     self.error(str(err))
-                
+
         namespace, args = super().parse_known_args(new_args, namespace)
         namespace_vars = vars(namespace)
 
@@ -67,7 +73,12 @@ class ArgumentParser(argparse.ArgumentParser):
             val = namespace_vars[key]
             if isinstance(val, str):
                 setattr(namespace, key, json.loads(val))
-        
+
+        for key in set(self._list_args.keys()) & set(namespace_vars.keys()):
+            val = namespace_vars[key]
+            if isinstance(val, str):
+                setattr(namespace, key, val.split(self._list_args[key]))
+
         for key in set(self._template_args) & set(namespace_vars.keys()):
             val = namespace_vars[key]
             if not val is None:
@@ -77,5 +88,5 @@ class ArgumentParser(argparse.ArgumentParser):
                         break
                     val = new_val
                 setattr(namespace, key, val)
-        
+
         return namespace, args
