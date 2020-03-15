9d13d58f1e8711813fd3f973d5fa4367b216ddf3
Omer Yair <omeryair@gmail.com>
2020-03-15 10:04:20 +0000

    Added Tensor2MultiGrid

--- gptb/visualizations.py
+++ gptb/visualizations.py
@@ -1,88 +1,144 @@
 import os
 import subprocess
-import copy
-import socket
 import time
 
 import numpy as np
+import torch
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import imageio
 import visdom
 
 def start_visdom(*args, port=9960, **kwargs):
-        try:
+    try:
         vis = visdom.Visdom(port=port, raise_exceptions=True, *args, **kwargs)
-        except:
-    print('\nRunning visdom at port {}\n'.format(port))
-    cmd = ['visdom',
-            '-p', str(port),
-            ]
-    subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    time.sleep(3)
-    vis = visdom.Visdom(port=port, *args, **kwargs)
+    except:
+        print('\nRunning visdom at port {}\n'.format(port))
+        cmd = ['visdom',
+               '-p', str(port),
+               ]
+        subprocess.Popen(cmd, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+        time.sleep(3)
+        vis = visdom.Visdom(port=port, *args, **kwargs)
 
     print('Connected to visdom at port {}\n'.format(port))
     return vis
 
+def gen_multigrid_indices(data_shape, borders=None, scale=1):
+    if len(data_shape) % 2 == 0:
+        data_shape = tuple(data_shape) + (1, )
+    data_shape = np.array(data_shape)
+    n_dims = len(data_shape)
+    n_levels = (n_dims - 1) // 2
+    height = data_shape[-3]
+    width = data_shape[-2]
+    channels = data_shape[-1]
+
+    # data_shape_no_scale = data_shape.copy()
+    # data_shape_scaled[-2] *= scale
+    # data_shape_scaled[-3] *= scale
+    # n_levels = len(data_shape_scaled) // 2
+
+    if borders is None:
+        borders = np.arange(n_levels - 1)[::-1]
+
+    def add_border_and_tile(img, reps, border_width):
+        img = np.concatenate((img, np.zeros((img.shape[0], border_width), dtype=bool)), axis=1)
+        img = np.concatenate((img, np.zeros((border_width, img.shape[1]), dtype=bool)), axis=0)  # pylint: disable=unsubscriptable-object
+        img = np.tile(img, reps)
+        img = img[:(img.shape[0] - border_width), :(img.shape[1] - border_width)]  # pylint: disable=unsubscriptable-object
+        return img
+
+    img = np.ones((height * scale, width * scale), dtype=bool)
+    for i_level in range(1, n_levels):
+        border_width = borders[-i_level]
+        level_height = data_shape[-(i_level * 2 + 3)]
+        level_width = data_shape[-(i_level * 2 + 2)]
+        img = add_border_and_tile(img, (level_height, level_width), border_width)
+
+    indices = np.repeat(img[:, :, None], channels, axis=2)
+
+    indices_img = np.reshape(np.arange(np.prod(data_shape)), data_shape)
+    indices_img = np.repeat(np.repeat(indices_img, scale, axis=n_dims - 2), scale, axis=n_dims - 3)
+    shuffle_indices = indices_img.transpose(tuple(np.arange(n_levels) * 2) + tuple(np.arange(n_levels) * 2 + 1) + (n_levels * 2,))
+    shuffle_indices = np.reshape(shuffle_indices, (np.prod(shuffle_indices.shape[:n_levels]),np.prod(shuffle_indices.shape[n_levels:(2 * n_levels)]), channels))
+
+    return indices, shuffle_indices
+
 class Images2MultiGrid:
-    def __init__(self, data_shape, borders=None):
-    
-        if len(data_shape) % 2 == 0:
-            data_shape = tuple(data_shape) + (1, )
-        data_shape = np.array(data_shape)
-        n_levels = len(data_shape) // 2
-        if borders is None:
-            borders = np.arange(n_levels - 1)[::-1]
-        
-        def add_border_and_tile(img, reps, border_width):
-            img = np.concatenate((img, np.zeros((img.shape[0], border_width), dtype=bool)), axis=1)
-            img = np.concatenate((img, np.zeros((border_width, img.shape[1]), dtype=bool)), axis=0)
-            img = np.tile(img, reps)
-            img = img[:(img.shape[0] - border_width), :(img.shape[1] - border_width)]
-            return img
-        
-        img = np.ones((data_shape[-3], data_shape[-2]), dtype=bool)
-        for i_level in range(1, n_levels):
-            border_width = borders[-i_level]
-            level_height = data_shape[-(i_level * 2 + 3)]
-            level_width = data_shape[-(i_level * 2 + 2)]
-            img = add_border_and_tile(img, (level_height, level_width), border_width)
-        
-        self._indices = np.repeat(img.flatten(), data_shape[-1])
-        self._shape = img.shape + (data_shape[-1],)
+    def __init__(self, data_shape, borders=None, scale=1):
+
+        self._indices, self._shuffle_indices = gen_multigrid_indices(data_shape, borders, scale)
+
+        self._shape = self._indices.shape
+        self._indices = self._indices.flatten()
+        self._shuffle_indices = self._shuffle_indices.flatten()
+
         self._img = np.zeros(self._shape)
-
-        indices_img = np.reshape(np.arange(np.prod(data_shape)), data_shape)
-        self._shuffle_indices = indices_img.transpose(tuple(np.arange(n_levels) * 2) + tuple(np.arange(n_levels) * 2 + 1) + (n_levels * 2,)).flatten()
-
         self(np.repeat(np.linspace(0, 1, np.prod(data_shape[:-1])), data_shape[-1]))
-    
-    def __call__(self, images):
-        self._img.flat[self._indices] = images.flat[self._shuffle_indices]
+
+    def __call__(self, imgs):
+        self._img.flat[self._indices] = imgs.flat[self._shuffle_indices]  # pylint: disable=unsupported-assignment-operation
         return self._img
-    
+
     @property
     def shape(self):
         return self._shape
-    
+
     @property
     def last_image(self):
         return self._img
 
+class Tensor2MultiGrid:
+    def __init__(self, data_shape, borders=None, scale=1, device_id='cpu'):
+
+        data_shape_np = data_shape[:-3] + data_shape[-2:] + data_shape[-3:-2]
+        self._indices, self._shuffle_indices = gen_multigrid_indices(data_shape_np, borders, scale)
+
+        tensor_indices = np.reshape(np.arange(np.prod(data_shape)), data_shape)
+        tensor_indices = np.moveaxis(tensor_indices, -3, -1)
+        self._shuffle_indices = tensor_indices.flat[self._shuffle_indices]
+
+        self._indices = np.moveaxis(self._indices, -1, -3)
+        self._shuffle_indices = np.moveaxis(self._shuffle_indices, -1, -3)
+
+        self._shape = self._indices.shape
+        self._indices = self._indices.flatten()
+        self._shuffle_indices = self._shuffle_indices.flatten()
+
+        self._img = torch.zeros(self._shape, device=device_id)
+        self(torch.repeat_interleave(torch.tensor(np.linspace(0, 1, np.prod(data_shape_np[:-1]), dtype=np.single), device=device_id).view(data_shape[:-3] + (1,) + data_shape[-2:]), data_shape[-3], dim=-3))  # pylint: disable=not-callable
+
+    def __call__(self, imgs):
+        # imgs = np.moveaxis(imgs.detach().cpu().numpy(), -3, -1)
+        # self._img.flat[self._indices] = imgs.flat[self._shuffle_indices]  # pylint: disable=unsupported-assignment-operation
+        # imgs = torch.tensor(self._img).permute(2, 0, 1)
+
+        self._img.view(-1)[self._indices] = imgs.view(-1)[self._shuffle_indices]
+        return self._img
+
+    @property
+    def shape(self):
+        return self._shape
+
+    @property
+    def last_image(self):
+        return self._img
+
+
 class MultiGridImagesViewer:
     def __init__(self, data_shape, borders=None, scale=1):
         self._grid_builder = Images2MultiGrid(data_shape, borders)
-        
+
         mpl_dpi = mpl.rcParams['figure.dpi']
-        self._fig = plt.figure(figsize=(self._grid_builder.shape[1] / mpl_dpi * scale, 
+        self._fig = plt.figure(figsize=(self._grid_builder.shape[1] / mpl_dpi * scale,
                                         self._grid_builder.shape[0] / mpl_dpi * scale))
         self._ax = self._fig.add_axes((0, 0, 1, 1))
         if self._grid_builder.shape[-1] == 1:
             self._img_image = self._ax.imshow(self._grid_builder.last_image[..., 0])
         else:
             self._img_image = self._ax.imshow(self._grid_builder.last_image)
-    
+
     def __call__(self, data):
         if self._grid_builder.shape[-1] == 1:
             self._img_image.set_data(self._grid_builder(data)[..., 0])
@@ -90,80 +146,6 @@ class MultiGridImagesViewer:
             self._img_image.set_data(self._grid_builder(data))
 
         self._fig.canvas.draw()
-
-class GridOfGridOfImages:
-    def __init__(self, shape,
-                 sub_border_width=0,
-                 border_width=1,
-                 ):
-    
-        self._shape = shape  # (grid_height, grid_width, sub_grid_height, sub_grid_width, image_height, image_width, n_channels)
-        self._sub_border_width = sub_border_width
-        self._border_width = border_width
-    
-        self._sub_border_v = np.ones((
-            self._shape[0], self._shape[1],
-            self._shape[2],
-            self._shape[4], self._sub_border_width,
-            self._shape[6]))
-
-        new_width = self._shape[3] * self._shape[5] + (self._shape[3] - 1) * self._sub_border_width
-
-        self._border_v = np.ones((
-            self._shape[0],
-            self._shape[2],
-            self._shape[4], self._border_width,
-            self._shape[6]))
-        
-        new_width = self._shape[1] * new_width + (self._shape[1] - 1) * self._border_width
-
-        self._sub_border_h = np.ones((
-            self._shape[0],
-            self._sub_border_width, new_width,
-            self._shape[6]))
-
-        new_height = self._shape[2] * self._shape[4] + (self._shape[2] - 1) * self._sub_border_width
-
-        self._border_h = np.ones((
-            self._border_width, new_width,
-            self._shape[6]))
-
-        new_height = self._shape[0] * new_height + (self._shape[0] - 1) * self._border_width
-
-        self._output_shape = (new_height, new_width, self._shape[6])
-    
-    def set(self, images):
-        images = np.reshape(np.array(images), self._shape)
-
-        images_list = [None] * (self._shape[3] * 2 - 1)
-        images_list[1::2] = [self._sub_border_v] * (self._shape[3] - 1)
-        images_list[::2] = [images[:, :, :, i, :, :, :] for i in range(self._shape[3])]
-        images = np.concatenate(images_list, axis=4)
-
-        images_list = [None] * (self._shape[1] * 2 - 1)
-        images_list[1::2] = [self._border_v] * (self._shape[1] - 1)
-        images_list[::2] = [images[:, i, :, :, :, :] for i in range(self._shape[1])]
-        images = np.concatenate(images_list, axis=3)
-
-        images_list = [None] * (self._shape[2] * 2 - 1)
-        images_list[1::2] = [self._sub_border_h] * (self._shape[2] - 1)
-        images_list[::2] = [images[:, i, :, :, :] for i in range(self._shape[2])]
-        images = np.concatenate(images_list, axis=1)
-
-        images_list = [None] * (self._shape[0] * 2 - 1)
-        images_list[1::2] = [self._border_h] * (self._shape[0] - 1)
-        images_list[::2] = [images[i, :, :, :] for i in range(self._shape[0])]
-        images = np.concatenate(images_list, axis=0)
-
-        self._image = images
-    
-    @property
-    def shape(self):
-        return self._output_shape
-    
-    @property
-    def image(self):
-        return self._image
 
 
 class GifWriter:
